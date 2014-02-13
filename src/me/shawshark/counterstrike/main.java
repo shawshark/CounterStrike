bf705c54362566aa544e8f939b834a2f7b79ab27
Chris <chrissbolding@gmail.com>
2014-02-13 11:03:12 +1100

    Create main.java

---
+++ src/me/shawshark/counterstrike/main.java
@@ -0,0 +1,10 @@
+package me.shawshark.counterstrike;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	public List<Arena> arenas = new ArrayList<Arena>();
+}

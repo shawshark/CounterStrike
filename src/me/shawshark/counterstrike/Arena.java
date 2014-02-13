92ad8be36a693eba136908bc3935869ddc814bfb
Chris <chrissbolding@gmail.com>
2014-02-13 12:02:58 +1100

    Update Arena.java

--- src/me/shawshark/counterstrike/Arena.java
+++ src/me/shawshark/counterstrike/Arena.java
@@ -3,21 +3,9 @@ package me.shawshark.counterstrike;
 import org.bukkit.event.Listener;
 
 public class Arena implements Listener {
-	
 	public String gametype;
 	public int arenaid;
 	public String name;
 	public int total_limit;
-	
-	/**
-	 *  @ffa - Free For All.
-	 *  Create new arena type
-	 */
-	
-	public Arena(String igametype, int iarenaid, String iname, int itotal_limit) {
-		gametype = igametype;
-		arenaid = iarenaid; 
-		name = iname;
-		total_limit = itotal_limit;
-	}
+	public Arena(String igametype, int iarenaid, String iname, int itotal_limit) { gametype = igametype; arenaid = iarenaid; name = iname; total_limit = itotal_limit; }
 }

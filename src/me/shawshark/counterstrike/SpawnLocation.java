146f699f68fdc917932eaa5792e709d35c99e406
Chris <chrissbolding@gmail.com>
2014-02-13 11:03:24 +1100

    Create SpawnLocation.java

---
+++ src/me/shawshark/counterstrike/SpawnLocation.java
@@ -0,0 +1,12 @@
+package me.shawshark.counterstrike;
+
+public class SpawnLocation {
+	
+	public String arena;
+	public int id;
+	
+	public SpawnLocation(String iarena, int iid) {
+		arena = iarena;
+		id = iid;
+	}
+}

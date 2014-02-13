6e99939a6cb1859370356fe8f975ab5be6d790c6
Chris <chrissbolding@gmail.com>
2014-02-13 21:57:52 +1100

    Create Sign.java

---
+++ src/me/shawshark/counterstrike/Sign.java
@@ -0,0 +1,9 @@
+package me.shawshark.counterstrike;
+
+import org.bukkit.Location;
+
+public class Sign {
+	public String arenaid;
+	public Location loc;
+	public Sign(String iarenaid, Location iloc) { arenaid = iarenaid; loc = iloc; }
+}

50ef66593b6bd749cfc046d254e15382d3a38f05
Chris <chrissbolding@gmail.com>
2014-02-13 12:01:28 +1100

    Update main.java

--- src/me/shawshark/counterstrike/main.java
+++ src/me/shawshark/counterstrike/main.java
@@ -6,5 +6,21 @@ import java.util.List;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
+	
 	public List<Arena> arenas = new ArrayList<Arena>();
+	public List<SpawnLocation> spawns = new ArrayList<SpawnLocation>();
+	
+	public LoadSave ls;
+	
+	public void onEnable() {
+		ls = new LoadSave(this);
+		
+		ls.loadArenas();
+		ls.loadSpawnLocations();
+	}
+	
+	public void onDisable() {
+		ls.saveArenas();
+		ls.SaveSpawnLocations();
+	}
 }
